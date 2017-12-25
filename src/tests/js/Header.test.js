d5075a26dcbc4425bc18d6a189b945aad259a9ff
Suzal3579 <pe.messh@gmail.com>
2017-12-25 16:43:31 +0530

    Single snapshot added ...

--- src/tests/js/Header.test.js
+++ src/tests/js/Header.test.js
@@ -6,6 +6,8 @@ import Header from "../../js/Header";
 test("should render header ", () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.find("h1").length).toBe(1);
+    expect(wrapper.find("h1").text()).toBe("Header");
+    expect(wrapper).toMatchSnapshot();
 });
 
 
