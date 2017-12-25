abfefd1f72e40ea0bbdbfaac35474c83e633a143
Suzal3579 <pe.messh@gmail.com>
2017-12-25 16:27:36 +0530

    All lol

---
+++ src/tests/js/Header.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Header from "../../js/Header";
+
+test("should render header ", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find("h1").length).toBe(1);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
