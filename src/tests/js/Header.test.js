15fd237f5db1d32adfc28eca1b742bf2b20853b4
Suzal3579 <pe.messh@gmail.com>
2017-12-25 16:51:46 +0530

    ToJSON from 'enzyme-to-json' added. Everything is working swell ...

--- src/tests/js/Header.test.js
+++ src/tests/js/Header.test.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 
 import Header from "../../js/Header";
 
+// toJSON use garrdai garrnu parrxa .. remember because jest.config.json file banako xaina maile ..
+// 119 enzyme ...
 test("should render header ", () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.find("h1").length).toBe(1);
     expect(wrapper.find("h1").text()).toBe("Header");
-    expect(wrapper).toMatchSnapshot();
+    expect(toJson(wrapper)).toMatchSnapshot();
 });
 
 
