2a5ab36ea3ebaeccfd2bac60429242a0ed16aca6
Suzal3579 <pe.messh@gmail.com>
2018-01-06 18:43:10 +0530

    adding firebase to the workflow !!

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+    apiKey: 'AIzaSyBn5fLcVSl4A6I-EUucf9m8d0ibZODWsjk',
+    authDomain: 'expensify-3c388.firebaseapp.com',
+    databaseURL: 'https://expensify-3c388.firebaseio.com',
+    projectId: 'expensify-3c388',
+    storageBucket: 'expensify-3c388.appspot.com',
+    messagingSenderId: '867239426865',
+};
+firebase.initializeApp(config);
