2efbc1f7144ab73c9fc13c1c3e50c0f3cdf07c09
Suzal3579 <pe.messh@gmail.com>
2018-01-17 22:26:59 +0530

    Trying semantic UI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './css/index.css';
 import App from './js/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import '../node_modules/semantic-ui-css/semantic.min.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
