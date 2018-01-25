07e3ad58cf3c73602775a4131a30969e25e64f87
Suzal3579 <pe.messh@gmail.com>
2018-01-25 22:33:01 +0530

    ExpenseSummary Working .... Firebase with expense summary working ... All working fine.

--- src/js/ExpensesSummary.js
+++ src/js/ExpensesSummary.js
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { database } from '../firebase/firebase';
 import getVisibleExpenses from '../apis/getVisibleExpenses';
 import expensesTotal from '../apis/expensesTotal';
 
-export const ExpensesSummary = ({ expenseCount, expenseTotal }) => {
-    const wordToDisplay = expenseCount === 1 ? 'expense' : 'expenses';
-    return (
-        <div>
-            {expenseCount > 0 ?
-                <h1>{expenseCount} {wordToDisplay} totalling : {expenseTotal}</h1> : undefined
-            }
-        </div>
-    );
-};
+export class ExpensesSummary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            expenseLength: undefined,
+            expenseTotal: undefined,
+        };
+    }
+    componentWillMount() {
+        database.ref('expenses')
+            .once('value')
+            .then((snapshot) => {
+                const dataReceived = snapshot.val();
+                if (dataReceived) {
+                    const expensesArrayFromFirebase =
+                        Object.keys(dataReceived)
+                            .map(a => [
+                                a,
+                                dataReceived[a].description,
+                                dataReceived[a].note,
+                                dataReceived[a].createdAt,
+                                dataReceived[a].amount]);
+                    let sum = 0;
+                    const expTotal = expensesArrayFromFirebase
+                        .map((val) => {
+                            sum += parseInt(val[4], 10);
+                            return sum;
+                        });
+                    this.setState(() => ({
+                        expenseLength: expensesArrayFromFirebase.length,
+                        expenseTotal: expTotal[expTotal.length - 1],
+                    }));
+                }
+            });
+    }
+    render() {
+        return (
+            <div>
+                <hr />
+                {this.state.expenseLength > 0 ?
+                    <h2 className="text-center">
+                        {this.state.expenseLength} expenses with {this.state.expenseTotal} rupees.
+                    </h2> :
+                    undefined
+                }
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = (state) => {
     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
